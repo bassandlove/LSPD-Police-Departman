1d2ee46fbcdae5d48c0709821660c62b3705ace2
bassandlove <kucko.suti@gmail.com>
2026-04-13 12:19:56 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Szolgálatban🚔",
         // Activity type number (0 = Playing).
         type: 0, 
       },
